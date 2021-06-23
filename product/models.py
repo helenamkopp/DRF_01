d03f66b500375f0d6bab522b6f64d0e55a720a69
Helena Kopp <helena.kopp@olist.com>
2021-06-23 14:55:19 -0300

    second commit

--- product/models.py
+++ product/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField('name', max_length=255),
-    description = models.CharField('description', max_length=255), 
-    price = models.FloatField('price'),
-    category = models.CharField('category', max_length=255),
-    brand = models.CharField('brand', max_length=80, default=''), 
+    name = models.CharField('name', max_length=70, default='')
+    description = models.CharField('description', max_length=70, default='') 
+    price = models.FloatField('price', default=0)
+    category = models.CharField('category', max_length=70, default='')
+    brand = models.CharField('brand', max_length=70, default='')
     gtin = models.CharField('gtin', max_length=70, default='')
